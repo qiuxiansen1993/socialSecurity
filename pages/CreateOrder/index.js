8ff151cf178cc15825f690b452397ce9d13614b5
qiuchenglin <qiuchenglin@meituan.com>
2021-07-27 02:15:47 +0800

    服务订单

--- pages/CreateOrder/index.js
+++ pages/CreateOrder/index.js
@@ -1,7 +1,12 @@
 import { GetRequest } from "../utils/tool";
 import { get, post } from "../utils/main";
-import { getSolution,calSbData,getRandomCompany } from "../utils/api/createOrder";
-import { getUserInfo } from '../utils/api/personal';
+import {
+  getSolution,
+  calSbData,
+  getRandomCompany,
+  getFeeInfo,
+} from "../utils/api/createOrder";
+import { getUserInfo } from "../utils/api/personal";
 import "../utils/main";
 import { wage, month } from "./config";
 import "./index.scss";
@@ -15,45 +20,117 @@ const calculateData = {
   gjjIsNew: true,
   addSalary: true,
 };
-const getUserInfoFunc = async()=>{
-    const { code, data } = await get(getUserInfo);
-    if(code === 200){
-        const {userName,userHouseHold,useIdCard} = data
-        document.getElementById('userName').innerHTML=userName;
-        document.getElementById('useIdCard').innerHTML=useIdCard;
-        document.getElementById('userHouseHold').innerHTML=userHouseHold;
-    }
-}
-const getRandomCompanyFunc = async()=>{
-    const { city } = GetRequest();
-    const { code, data } = await get(getRandomCompany,{
-        // city
-    });
-    if(code === 200){
-        const {cityName,corpName} = data
-        document.getElementById('cityName').innerHTML=cityName;
-        document.getElementById('corpName').innerHTML=corpName;
-    }
-}
-const calSbDataFunc = async() =>{
-    [...document.querySelectorAll('.seleted-checkbox')].forEach((item)=>{
-        calculateData[item.name] = item.checked
-    })
-    const { code, data } = await post(calSbData,{
-        ...calculateData,
-        baseSalary:configData.wage,
-        startMonth:configData.startMonth,
-    });
+const serviceData = {};
+let FeeInfo = [];
+let canSelectWage = true;
+let serverCostIdx = 0;
+let mainTotleCost = 0;// 社保+公积金+工资总数
+const getUserInfoFunc = async () => {
+  const { code, data } = await get(getUserInfo);
+  if (code === 200) {
+    const { userName, userHouseHold, useIdCard } = data;
+    document.getElementById("userName").innerHTML = userName;
+    document.getElementById("useIdCard").innerHTML = useIdCard;
+    document.getElementById("userHouseHold").innerHTML = userHouseHold;
+  }
+};
+const getRandomCompanyFunc = async () => {
+  const { city } = GetRequest();
+  const { code, data } = await get(getRandomCompany, {
+    // city
+  });
+  if (code === 200) {
+    const { cityName, corpName } = data;
+    document.getElementById("cityName").innerHTML = cityName || "";
+    document.getElementById("corpName").innerHTML = corpName || "";
+  }
+};
+const getFeeInfoFunc = () => {
+  return new Promise(async (resove, reject) => {
+    const { code, data } = await post(getFeeInfo, {});
     if (code === 200) {
-        //跳转
-      }
+      resove(data);
+      document.getElementById("service-charge-info").innerHTML = data[0]?.sum;
+      FeeInfo = data;
+    } else {
+      reject(null);
+    }
+  });
+};
+const calSbDataFunc = async () => {
+  getFeeInfoFunc();
+  [...document.querySelectorAll(".seleted-checkbox")].forEach((item) => {
+    calculateData[item.name] = item.checked;
+  });
+  const { code, data } = await post(calSbData, {
+    ...calculateData,
+    baseSalary: configData.wage,
+    startMonth: configData.startMonth,
+  });
+  const feeData = await getFeeInfoFunc();
+  if (code === 200) {
+    //跳转
+    canSelectWage = false;
+    document.querySelector(".create-order-seleted").style = "display:none;";
+    document.querySelector(".submit-content").style = "display:block;";
+    // 展示
+    const { sb, gjj, salary } = data;
+    mainTotleCost = salary.total + gjj.total + salary.total
+    document.getElementById("sb-cost").innerHTML = sb.total;
+    document.getElementById("gjj-cost").innerHTML = gjj.total;
+    document.getElementById("salary-cost").innerHTML = salary.total;
+    document.getElementById("totle-cost").innerHTML =
+    mainTotleCost + (feeData?.[0]?.sum || 0);
+      initSubmitEvent();
+  }
+};
+const renderServerChargeLi = ()=>{
+  const render = FeeInfo.map((item,index)=>{
+    return `<li data-sum="${item.sum}" data-idx="${index}" class="mui-table-view-cell ${index===Number(serverCostIdx)&&'mui-selected'}"><a class="mui-navigate-right"><div style="display:flex;justify-content: space-between;"><div style="text-align:left;"><span>${item.title}</span><p>${item.fromM2M}</p></div><div><span style="color:lightcoral;">${item.sum}元</span></div></div></a></li>`
+  }).join('')
+  return render
 }
+const initSubmitEvent = () => {
+  document
+    .querySelector(".cost-view")
+    .addEventListener("tap", function (event) {
+      mui.alert(`
+    <div style="text-align:left;">
+    <p style="font-size:16px;text-align:center;">业务办理说明</p>
+    <p>1、五险一金需同时缴纳</p>
+    <p>2、请上传有效的身份证反正面</p>
+    <p>3、如有问题请致电我们</p>
+    </div>
+    `);
+    });
+  document
+    .getElementById("service-charge")
+    .addEventListener("tap", function (event) {
+      mui.confirm(`
+      <ul class="mui-table-view mui-table-view-radio service-charge-sel">${renderServerChargeLi()}</ul>
+    `);
+    var list = document.querySelector('.mui-table-view.mui-table-view-radio');
+    list.addEventListener('selected',function(e){
+      serverCostIdx = e.detail.el.getAttribute('data-idx')
+      document.getElementById("totle-cost").innerHTML = mainTotleCost+Number(e.detail.el.getAttribute('data-sum'))
+    }); 
+    });
+};
+
 const getSolutionFunc = async () => {
   const { code, data } = await get(getSolution);
   console.log(code, data);
   if (code === 200) {
-    //
-    document.querySelector('.create-order-seleted').style="display:block;"
+    const { companyGjj, personGjj, baseGjj } = data;
+    const { city } = GetRequest();
+    document.querySelector(".create-order-seleted").style = "display:block;";
+    document.getElementById("sbPlan").innerHTML = `${city}职工社保`;
+    document.getElementById("sbBase").innerHTML = "";
+    document.getElementById("gjjPlan").innerHTML =
+      city + "公积金(" + companyGjj * 100 + "%-" + personGjj * 100 + "%" + ")";
+    document.getElementById("gjjBase").innerHTML = baseGjj;
+    document.getElementById("xzPlan").innerHTML = `${city}个税`;
+    document.getElementById("gongziDate").innerHTML = "当月25日";
     initSwitchFunc();
   }
 };
@@ -66,6 +143,10 @@ const initPicker = (buttonId, resultId, data) => {
   showUserPickerButton.addEventListener(
     "tap",
     function (event) {
+      if (!canSelectWage) {
+        mui.toast("标准和日期不可修改");
+        return;
+      }
       picker.show(function (items) {
         result.innerText = items[0];
         configData[resultId] = items[0];
@@ -84,7 +165,9 @@ const initSwitchFunc = () => {
     .addEventListener("toggle", function (event) {
       if (event.detail.isActive) {
         console.log("你启动了开关");
+        calculateData["sbIsNew"] = false;
       } else {
+        calculateData["sbIsNew"] = true;
         console.log("你关闭了开关");
       }
     });
@@ -92,23 +175,23 @@ const initSwitchFunc = () => {
     .getElementById("gongjijin-switch")
     .addEventListener("toggle", function (event) {
       if (event.detail.isActive) {
+        calculateData["gjjIsNew"] = false;
         console.log("你启动了开关");
       } else {
+        calculateData["gjjIsNew"] = true;
         console.log("你关闭了开关");
       }
     });
-    document
+  document
     .getElementById("calculate-btn")
     .addEventListener("tap", function (event) {
-        calSbDataFunc();
+      calSbDataFunc();
     });
-    
 };
 const initCreateOrder = () => {
-    getUserInfoFunc();
+  getUserInfoFunc();
   getRandomCompanyFunc();
   initPicker("paymonth", "month", month);
   initPicker("paywage", "wage", wage);
-  //initSwitchFunc();
 };
 initCreateOrder();
