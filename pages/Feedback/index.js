693defc4c4043627f51242a3d258140833c57cc4
shizhongwei <shizhongwei01@58ganji.com>
2021-08-15 17:03:13 +0800

    提交帮助中心

--- pages/Feedback/index.js
+++ pages/Feedback/index.js
@@ -1,9 +1,2 @@
 import "../utils/main"
 import './index.scss';
-document.getElementById("view-order").addEventListener("tap", function () {
-    window.location = `${document.location.protocol}//${window.location.host}/PayCost/index.html`
-  });
-document.getElementById("updata-indo").addEventListener("tap", function () {
-    window.location = `${document.location.protocol}//${window.location.host}/MyInfo/index.html?back=Home`
-});
-  
